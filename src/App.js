b5ef0439a97f6a8cbae2bf684bc37fd43a4a9905
Twel Tar <tweltar@Twels-MacBook-Pro.local>
2020-09-24 17:15:39 +0630

    add additional dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import SideNav from "./components/SideNav";
+import recreateMuiTheme from "./theme/recreateMuiTheme";
+import { colorValues, modes } from "./utils";
+import { ThemeProvider } from "@material-ui/core";
 
 function App() {
-  return <div className="App"></div>;
+  const [open, setOpen] = useState(false);
+  const [mode, setMode] = useState(modes.DARK);
+  const [pcolor, setPcolor] = useState(colorValues.BLACK);
+
+  const customizedTheme = recreateMuiTheme(mode, pcolor);
+
+  return (
+    <ThemeProvider theme={customizedTheme}>
+      <div className="App">
+        <SideNav />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage}></Route>
+            <Route exact path="/home" component={HomePage}></Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
